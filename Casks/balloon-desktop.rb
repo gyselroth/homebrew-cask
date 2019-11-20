f9b44e6f654b563be461a0489321aa0ac529620d
Raffael Sahli <sahli@gyselroth.com>
2019-11-20 14:40:36 +0100

    balloon-desktop

---
+++ Casks/balloon-desktop.rb
@@ -0,0 +1,12 @@
+cask 'balloon-desktop' do
+  version '1.1.1'
+  sha256 '80fd47159d8a4cfdd865ae283835b48365a5e455f6770521a9c09c123e2324f2'
+
+  url "https://github.com/gyselroth/balloon-client-desktop/releases/download/v1.1.1/balloon-desktop-1.1.1.pkg"
+  name 'balloon desktop client'
+  homepage 'https://github.com/gyselroth/balloon-client-desktop'
+
+  pkg "Double-Click to Install Zulu balloon-desktop-1.1.1.pkg"
+
+  uninstall pkgutil: "balloon-desktop-1.1.1"
+end
