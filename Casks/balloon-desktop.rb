3c19c36eab04169d68f629efaa23558a0ec7e8c6
Travis CI <travis@travis-ci.org>
2020-02-24 12:20:31 +0000

    balloon-desktop release 1.2.2

--- Casks/balloon-desktop.rb
+++ Casks/balloon-desktop.rb
@@ -1,12 +1,12 @@
 cask 'balloon-desktop' do
-  version "1.2.1"
-  sha256 "b811ea6e8dced7afbb3251a058e49f65864b37eadb82eb78781dfab596839404"
+  version "1.2.2"
+  sha256 "bb890d1079c7e87fd0fff8595161f319d011ea8a14618b3d559350f24a20be35"
 
-  url "https://github.com/gyselroth/balloon-client-desktop/releases/download/v1.2.1/balloon-desktop-1.2.1.pkg"
+  url "https://github.com/gyselroth/balloon-client-desktop/releases/download/v1.2.2/balloon-desktop-1.2.2.pkg"
   name 'balloon desktop client'
   homepage 'https://github.com/gyselroth/balloon-client-desktop'
 
-  pkg "balloon-desktop-1.2.1.pkg"
+  pkg "balloon-desktop-1.2.2.pkg"
 
-  uninstall pkg "balloon-desktop-1.2.1.pkg"
+  uninstall pkg "balloon-desktop-1.2.2.pkg"
 end
